02724e7a2ddd98ee2c0e12571b395e4678abd299
parkjiu0208 <joy002208@gmail.com>
2023-08-23 18:24:57 +0900

    day3_1

---
+++ Python/study/DAY3/day3_1.py
@@ -0,0 +1,16 @@
+def solution(a, b):
+    a = f"{a}"
+    b = f"{b}"
+    if int(a+b) < int(b+a):
+        return int(b+a)
+    else:
+        return int(a+b)
+    answer = 0
+    return answer
+
+print(solution(9,91))
+
+# 다른 사람의 풀이
+#  def solution(a, b):
+#      return int(max(f"{a}{b}", f"{b}{a}"))
+# 답이 so 명료...ㅜㅜ 더 노력하자...
