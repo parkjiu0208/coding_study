fc5805330d03b7ed67478367addfa6b8d3ec3a99
parkjiu0208 <joy002208@gmail.com>
2023-09-13 17:22:54 +0900

    day13_1

---
+++ Python/study/DAY13/day13_1.py
@@ -0,0 +1,22 @@
+def solution(str_list):
+    for i in str_list:
+        if i == "l":
+            return str_list[:str_list.index("l")]
+            break
+        elif i == "r":
+            return str_list[str_list.index("r"):]
+            break
+        elif "l" not in str_list and "r" not in str_list:
+            return []
+            
+
+'''
+def solution(str_list):
+    for li in str_list:
+        if li == 'l':
+            return str_list[:str_list.index('l')]
+        elif li == 'r':
+            return str_list[str_list.index('r')+1:]
+
+-> 이건 기원오빠 코드. +1을 안한 걸 오빠 코드 통해서 알았다... ㅠ 한끝차이
+'''
