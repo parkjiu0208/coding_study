494576d8ff06e288136d28cd643bf5e41c445b3d
박지우 <121562687+parkjiu0208@users.noreply.github.com>
2024-01-30 15:30:15 +0900

    [level 1] Title: 평균 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
@@ -1,5 +1,7 @@
+# def solution(arr):
+#     answer = 0
+#     for i in arr:
+#         answer+=i
+#     return answer/len(arr)
 def solution(arr):
-    answer = 0
-    for i in arr:
-        answer+=i
-    return answer/len(arr)
+    return sum(arr)/len(arr)
