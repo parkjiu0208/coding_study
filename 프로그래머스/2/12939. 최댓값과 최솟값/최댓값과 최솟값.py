c466d1f5bece58298921f3f258748009cfe2e604
박지우 <121562687+parkjiu0208@users.noreply.github.com>
2024-10-03 00:08:38 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,7 +1,4 @@
 def solution(s):
-    answer = ''
     s = s.split(' ')
     a = list(map(int, s))
-    answer += str(min(a))+' '
-    answer += str(max(a))
-    return answer
+    return str(min(a))+' '+str(max(a))
